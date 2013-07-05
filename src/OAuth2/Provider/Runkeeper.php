ad69b0920acc6824fe6011310c90f190a38fede3
Erik Yeoh <erik.yeoh@wyred.nu>
2013-07-05 16:15:05 +0800

    Fix undefined namespace error

--- src/OAuth2/Provider/Runkeeper.php
+++ src/OAuth2/Provider/Runkeeper.php
@@ -3,7 +3,7 @@
 namespace OAuth2\Provider;
 
 use OAuth2\Provider;
-use OAuth2\Token\Token_Access;
+use OAuth2\Token_Access;
 
 /*
  * RunKeeper API credentials: http://runkeeper.com/partner/applications/view
