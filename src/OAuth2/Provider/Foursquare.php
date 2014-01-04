d4adbc8442cb33373a2e7a5aa99f29a1585e99a7
Mathijs <mathijs@mathijshuis.nl>
2014-01-04 18:04:59 +0100

    Added method get_user_information() and get_user() for Foursquare provider

--- src/OAuth2/Provider/Foursquare.php
+++ src/OAuth2/Provider/Foursquare.php
@@ -20,4 +20,49 @@ class Foursquare extends Provider
 	{
 		return 'https://foursquare.com/oauth2/access_token';
 	}
+
+	public function get_user_info(Token_Access $token)
+	{ 
+		$url = 'https://api.foursquare.com/v2/users/self?'.http_build_query(array(
+			'oauth_token' => $token->access_token,
+			'v' => '20140104',
+		));
+
+		$user = json_decode(file_get_contents($url));
+
+		if (isset($user) && ($user->meta->code == 200))
+		{
+			return array(          
+				'uid' => $user->response->user->id,
+				'firstname' => $user->response->user->firstName,
+				'lastname' => $user->response->user->lastName,
+				'name' => sprintf('%s %s', $user->response->user->firstName, $user->response->user->lastName),
+				'email' => (isset($user->response->user->contact->email)) ? $user->response->user->contact->email : '',
+			);
+		} 
+		else
+		{
+			return null;
+		}
+	}
+
+	public function get_user(Token_Access $token)
+	{
+
+		$url = 'https://api.foursquare.com/v2/users/self?'.http_build_query(array(
+			'oauth_token' => $token->access_token,
+			'v' => '20140104',
+		));
+
+		$user = json_decode(file_get_contents($url),true);
+
+		if (isset($user) && ($user['meta']['code'] == 200))
+		{
+			return $user['response']['user'];
+		} 
+		else
+		{
+			return null;
+		}
+	}
 }
